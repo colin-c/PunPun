e98f7bb0e19e75bf2868018df70068ddf464e0c8
Colin Chen <colin.cchn@gmail.com>
2023-07-01 23:54:50 -0700

    edit

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ from discord.ext import commands
 load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
-intents = discord.Intents.default()
+intents = discord.Intents.all()
 intents.message_content = True
 
 client = commands.Bot(command_prefix='b! ', intents=intents)
@@ -27,12 +27,6 @@ client = commands.Bot(command_prefix='b! ', intents=intents)
 @client.event
 async def on_ready():
     print('Connection is Sucessful!')
-
-@client.event
-async def on_message(message):
-    #channel = client.get_channel(1025140342263119974)
-    #await channel.send('testing')
-    print(message.author, message.content, message.channel.id)
 
 async def load():
     for filename in os.listdir("./commands"):
