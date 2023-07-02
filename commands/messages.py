394ec5f27770e8cf18e9aa399fcdd990d1f344ef
Colin Chen <colin.cchn@gmail.com>
2023-07-02 03:36:04 -0700

    updated coin.py, added gif.txt and memes.py

--- commands/messages.py
+++ commands/messages.py
@@ -1,5 +1,4 @@
 import os
-import discord
 from dotenv import load_dotenv
 from discord.ext import commands
 
@@ -25,13 +24,6 @@ class Messages(commands.Cog):
     async def goodbye(self, ctx):
         channel = self.client.get_channel(DISCORD_SERVER_ID)
         await channel.send(f'I hope to see you again {ctx.author.mention}!')
-    
-    @commands.command(aliases=["ily"])
-    async def iloveyou(self, ctx):
-        iloveyou_message = discord.Embed(title=f"I love you too *{ctx.author.name}*", color=discord.Color.pink())
-        iloveyou_message.set_image(url="https://preview.redd.it/cntkwp9wthb31.jpg?auto=webp&s=f4441eeb8bb1d77fe7e4c314ea7030b1eb93c29a")
-        
-        await ctx.send(embed = iloveyou_message)
 
 async def setup(client):
     await client.add_cog(Messages(client))
