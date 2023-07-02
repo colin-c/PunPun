fd8e2bd2de15c41c267c16040483c2030cdfed64
Colin Chen <colin.cchn@gmail.com>
2023-07-01 23:38:45 -0700

    initial commit

---
+++ bot.py
@@ -0,0 +1,42 @@
+#### TESTING to get the channel ID 
+
+## channel Test bot server: 1025140342263119974
+
+# @client.event
+# async def on_message(message):
+#     #channel = client.get_channel(1025140342263119974)
+#     #await channel.send('testing')
+#     print(message.author, message.content, message.channel.id)
+
+import discord
+import os
+import asyncio
+
+from dotenv import load_dotenv
+from discord.ext import commands
+
+
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = commands.Bot(command_prefix='b! ', intents=intents)
+
+@client.event
+async def on_ready():
+    print('Connection is Sucessful!')
+
+async def load():
+    for filename in os.listdir("./commands"):
+        if filename.endswith(".py"):
+            await client.load_extension(f"commands.{filename[:-3]}")
+
+
+async def main():
+    async with client:
+        await load()
+        await client.start(TOKEN)
+
+asyncio.run(main())
