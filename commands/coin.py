34988cabf286d082ca5e3baa8c74c3baa13ec388
Colin Chen <colin.cchn@gmail.com>
2023-07-02 03:08:08 -0700

    changed coin method in coin.py

--- commands/coin.py
+++ commands/coin.py
@@ -1,5 +1,6 @@
 import os
 import random
+import discord
 from dotenv import load_dotenv
 from discord.ext import commands
 
@@ -19,9 +20,16 @@ class Coin(commands.Cog):
 
     @commands.command()
     async def coin(self, ctx):
-        channel = self.client.get_channel(DISCORD_SERVER_ID)
         result = random.choice(["Head", "Tail"])
-        await channel.send(f'{result}')
+        
+        if (result == "Head"):
+            head_message = discord.Embed(title="HEAD", color=discord.Color.green())
+            head_message.set_image(url="https://ih1.redbubble.net/image.2982672877.3829/poster,504x498,f8f8f8-pad,600x600,f8f8f8.jpg")
+            await ctx.send(embed = head_message)
+        else:
+            tail_message = discord.Embed(title="TAIL", color=discord.Color.red())
+            tail_message.set_image(url="https://i.kym-cdn.com/photos/images/original/000/996/823/191.jpg")
+            await ctx.send(embed = tail_message)
 
 async def setup(client):
     await client.add_cog(Coin(client))
