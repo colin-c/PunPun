8a2eaa0b7dfb7ba16807fc5946f09732c737a94c
Colin Chen <colin.cchn@gmail.com>
2023-07-02 17:41:18 -0700

    change fortuneCookie to embed format

--- commands/fortuneCookie.py
+++ commands/fortuneCookie.py
@@ -1,5 +1,6 @@
 import os
 import random
+import discord
 from dotenv import load_dotenv
 from discord.ext import commands
 
@@ -16,14 +17,34 @@ class FortuneCookie(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         print("Fortune Cookie File Loaded")
-    
+
     @commands.command(aliases=["fc"])
     async def fortuneCookie(self, ctx):
-        channel = self.client.get_channel(DISCORD_SERVER_ID)
-        with open("commands/fcmsg.txt", "r") as f:
-            randomResponses = f.readlines()
-            response = random.choice(randomResponses)
-        await channel.send(f'{response}')
+        result = random.choice(["Good", "Bad"])
+        
+        if (result == "Good"):
+            with open("commands/fcGood.txt", "r") as f:
+                randomResponses = f.readlines()
+                response = random.choice(randomResponses)
+
+            fcGood_message = discord.Embed(title="Good", description=response, color=discord.Color.green())
+            await ctx.send(embed = fcGood_message)
+        elif (result == "Bad"):
+            with open("commands/fcBad.txt", "r") as f:
+                randomResponses = f.readlines()
+                response = random.choice(randomResponses)
+
+            fcBad_message = discord.Embed(title="Bad", description=response, color=discord.Color.red())
+            await ctx.send(embed = fcBad_message)
+    
+    # @commands.command(aliases=["fc"])
+    # async def fortuneCookie(self, ctx):
+    #     channel = self.client.get_channel(DISCORD_SERVER_ID)
+
+    #     with open("commands/fcmsg.txt", "r") as f:
+    #         randomResponses = f.readlines()
+    #         response = random.choice(randomResponses)
+    #     await channel.send(f'`{response}`')
 
 async def setup(client):
     await client.add_cog(FortuneCookie(client))
